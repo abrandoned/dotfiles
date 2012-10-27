80eef09b21b669c8087dccef17143280fef05581
Brandon Dewitt <bdewitt@bdewitt-left.(none)>
2012-10-27 14:59:16 -0600

    update path to git completion

--- git/completion.bash
+++ git/completion.bash
@@ -1,7 +1,6 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-completion=/usr/local/etc/bash_completion.d/git-completion.bash
+# bash `git-completion` script at $completion below
+completion=/etc/bash_completion.d/git
 
 if test -f $completion
 then
